7be631d4b9b38817ef368c8c7523d9b753e62e8e
Zhao Li <zhao.lang.li2@gmail.com>
2018-06-20 12:02:29 -0500

    initial mcmc file commit

---
+++ multiMCMC.m
@@ -0,0 +1,57 @@
+%Multivariate MCMC
+clear
+
+%function for analytic value
+f=@(x) .5*log(2*pi*exp(1)*(x));
+
+%initialization
+sampleSize=9000;
+%dimensions>1
+dim=5;
+mean=0;
+sd=1;
+start=zeros(1, dim);
+
+%target distribution, [chainSize 1] vector
+pdf=@(x) mvnpdf(x, zeros(1,dim), eye(dim));
+%proposal pdf, [chainSize 1] vector
+proppdf=@(x,y) prod(mvnpdf(mean, sd));
+%random number generator, [chainSize dim] matrix
+proprnd=@(x) normrnd(mean, sd, 1, dim);
+
+%gather samples, returns size [sampleSize dim]
+sample=mhsample(start, sampleSize, 'logpdf', pdf, 'proppdf', proppdf, 'proprnd', proprnd);
+
+c=cov(sample)
+determinant=det(c)
+f(determinant)
+
+%initialize temp to hold samples
+temp=zeros(sampleSize, 1);
+
+%for each sample
+for i=1:sampleSize
+    %calculate covariance and determinant of preceding samples
+    sam=sample(1:i, :);
+    size(sam);
+    co=cov(sam);
+    d=det(co);
+    temp(i)=f(d);
+end
+%analytic value
+sdi=eye(dim)*sd^2;
+analyticVal=f(det(sdi));
+
+%figure;
+%hist(sample, 50);
+
+%plotting f(x) at each sample
+figure;
+plot(1:sampleSize, temp);
+hold on;
+plot([0 sampleSize], [analyticVal analyticVal]);
+hold off;
+title(['Mean: ', num2str(mean),', SD: ', num2str(sd),', Dimensions: ', num2str(dim)]);
+ylabel('f(x)');
+xlabel('Sample Size');
+legend({strcat('AV: ', num2str(analyticVal))}, 'FontSize', 12, 'TextColor', 'blue');
---
+++ uniMCMC.m
@@ -0,0 +1,47 @@
+%Univariate MCMC
+clear
+
+%function for analytic value
+f=@(x) .5*log(2*pi*exp(1)*(x));
+
+%initialization
+sampleSize=10000;
+mean=0;
+sd=2;
+%target distribution
+pdf=@(x) normpdf(x, mean, sd+.1);
+%proposal pdf
+proppdf=@(x, y) normpdf(x, mean, sd);
+%random number generator
+proprnd=@(x) normrnd(mean, sd);
+
+%gather samples
+sample=mhsample(1, sampleSize, 'pdf', pdf, 'proprnd', proprnd, 'proppdf', proppdf);
+
+%initialize temp to hold samples
+temp=sample;
+
+%for each sample
+for i=1:sampleSize
+    %calculate variance of all preceding samples
+    sam=sample(1:i, 1);
+    temp(i)=f(var(sam));
+end
+variance=var(sample);
+%analytic value
+analyticVal=f(sd^2);
+
+%histogram to show normal distribution of samples
+%figure;
+%hist(sample, 100);
+
+%plotting f(x) at each sample
+figure;
+plot(1:sampleSize, temp);
+hold on;
+plot([0 sampleSize], [analyticVal analyticVal])
+hold off;
+title(['Mean: ', num2str(mean),', SD: ', num2str(sd),', Proposal: Norm']);
+ylabel('f(x)');
+xlabel('Sample Size');
+legend({strcat('AV: ', num2str(analyticVal))}, 'FontSize', 12, 'TextColor', 'blue')
