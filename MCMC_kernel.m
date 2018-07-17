8566d41a7788dac9120d735fc0acdd95679f006f
Frank Li <zhao.lang.li2@gmail.com>
2018-07-17 17:20:32 -0500

    etc

--- MCMC_kernel.m
+++ MCMC_kernel.m
@@ -1,13 +1,12 @@
 
 %init
 load MI_QALAS_objfun_kernel_input.mat;
-sampleSize=1000;
+sampleSize=100;
 dim=9;
-dims=1:9;
-i=1;
+i=1; j=1;
 eta=zeros(1, dim);
 
-ksrtemp=zeros(5,151,181);
+ksrtemp=zeros(5, 151, 181);
 ksitemp=zeros(5, 151, 181);
 
 %prior mean and sd
@@ -20,6 +19,11 @@ tsigma=eta_target(:, 2);
 samples=zeros(sampleSize, dim);
 MItemp=zeros(1, sampleSize);
 
+%to store vals causing -Inf
+infeta=zeros(100, 9);
+infksr=zeros(5, 151, 181);
+infksi=zeros(5, 151, 181);
+
 while i<sampleSize
     %increment
     i=i+1;
@@ -27,17 +31,31 @@ while i<sampleSize
     for j=1:dim
         %sample from proposal probability distribution
         sample=normrnd(samples(i-1, j), psigma(j));
-        samples(i, j)=metHas(sample, samples(i-1, j), tmu(j), tsigma(j));
+        samples(i, j)=metHas(sample, samples(i-1, j), tmu(j), .5);
     end
+    %set positive eta values
     eta=abs(samples(i, :));
+    %calc and sum ksr and ksi
     [ksr, ksi]=MI_QALAS_objfun_kernel(eta);
     ksrtemp=ksrtemp+ksr;
     ksitemp=ksitemp+ksi;
     
-    [MI, MIimg]=calcMI(ksrtemp, ksitemp, i);
+    [MI, ~]=calcMI(ksrtemp, ksitemp, i);
     MItemp(i)=MI;
+    
+    if isinf(MI)
+        infksr=ksrtemp;
+        infksi=ksitemp;
+        infeta(j, :)=eta;
+        j=j+1;
+        break;
+    end
 end
 
+[MI, MIimg]=calcMI(ksrtemp, ksitemp, sampleSize);
+
+%display img
+figure;
 imagesc(squeeze(MIimg(1, :, :)));
 
 %plot MI at each sampleSize
@@ -47,36 +65,33 @@ plot(1:sampleSize, MItemp);
 hold on;
 plot([0 sampleSize], [analyticVal analyticVal])
 hold off;
-title(['MI vs Sample Size']);
+title(strcat('MI: (', num2str(MI), ') vs Sample Size: (', num2str(sampleSize), ')'));
 ylabel('MI');
 xlabel('Sample Size');
 legend({strcat('AV: ', num2str(analyticVal))}, 'FontSize', 12, 'TextColor', 'blue')
 
-
-%{
-for i=1:5
-   figure;
-   imagesc(squeeze(MIimg(i, :, :)));
-end
-%}
-
-%{
+%display histogram of samples
 figure;
 for i=1:dim
     subplot(4, 4, i);
+    %leftover zeros from init***
     hist(samples(:, i), 20);
     title(strcat('tmu: ', num2str(tmu(i)), ' tsigma: ', num2str(tsigma(i))));
     xlabel(strcat('std: ', num2str(std(samples(:, i))), ' mean: ', num2str(mean(samples(:, i)))));
 end
-%}
 
+%Metropolis-Hastings Algo
 function val=metHas(sample, prev, mu, sigma)
     %target distribution
-    f=@(x) normpdf(x, mu, sigma);
+    tpdf=@(x) normpdf(x, mu, sigma);
+    %proposal distribution
+    ppdf=@(x, y) normpdf(x, y);
     %draw random number
     r=rand();
+    %corrective ratio
+    c=ppdf(prev, sample)/ppdf(sample, prev);
     %determine acceptance
-    alpha=min([1, f(sample)/f(prev)]);
+    alpha=min([1, tpdf(sample)/tpdf(prev)*c]);
     
     if (r<=alpha)
         %accept proposal
@@ -87,8 +102,9 @@ function val=metHas(sample, prev, mu, sigma)
     end
 end
 
+%calculate MI/MIimg
 function [MI, MIimg]=calcMI(ksr, ksi, sampleSize)
-load MI_QALAS_objfun_kernel_input.mat;
+load MI_QALAS_objfun_kernel_input.mat; %#ok<LOAD>
 Er=ksr/sampleSize;
 Ei=ksi/sampleSize;
 Sr=(ksr.^2)/sampleSize;
@@ -104,7 +120,7 @@ MIimg=Hz-Hzmu;
 
 szmi=size(MIimg);
 pdv=1;
-szmi(2:1+ndims(subsmplmask{pdv}))=1;
+szmi(2:1+ndims(subsmplmask{pdv}))=1; %#ok<NODEF>
 subsmplmask{pdv}=permute(subsmplmask{pdv},[ndims(subsmplmask{pdv})+1,1:ndims(subsmplmask{pdv})]);
 subsmplmask{pdv}=repmat(subsmplmask{pdv},szmi);
 MI=sum(MIimg(:).*subsmplmask{pdv}(:));
